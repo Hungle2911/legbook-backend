05dd05a1491ca9edfb980e4a1cbf828ccc393da0
Hungle2911 <122929293+Hungle2911@users.noreply.github.com>
2024-11-03 14:42:26 -0500

    set up middleware for server

--- src/setupServer.ts
+++ src/setupServer.ts
@@ -1,5 +1,13 @@
 import {Application, json, urlencoded, Response, Request, NextFunction} from "express";
 import http from "http";
+import cors from "cors"
+import helmet from "helmet";
+import compression = require("compression")
+import hpp from "hpp";
+import morgan from "morgan";
+import cookieSession from "cookie-session";
+import HTTP_STATUS from "http-status-codes";
+import "express-async-errors"
 
 export class Server {
   private app: Application;
@@ -15,9 +23,30 @@ export class Server {
     this.startServer(this.app);
   }
 
-  private securityMiddlewares(app: Application): void {}
+  private securityMiddlewares(app: Application): void {
+    app.use(
+      cookieSession({
+        name: "session",
+        keys: ["key1", "key2"],
+        maxAge: 24 * 60 * 60 * 1000,
+        secure: false,
+      }),
+    );
+    app.use(cors(
+      {
+        origin: "http://localhost:5173",
+        credentials: true,
+      },
+    ));
+    app.use(helmet());
+  }
 
-  private standardMiddlewares(app: Application): void {}
+  private standardMiddlewares(app: Application): void {
+    app.use(compression());
+    app.use(json());
+    app.use(urlencoded({ extended: true }));
+    app.use(morgan("dev"));
+  }
 
   private routeMiddlewares(app: Application): void {}
 
