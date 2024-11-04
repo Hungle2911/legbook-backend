f9e147336fccc421ea83b7a99342759d978358a6
Hungle2911 <122929293+Hungle2911@users.noreply.github.com>
2024-11-04 00:08:42 -0500

    add message when connect to database successfully

--- src/setupDatabases.ts
+++ src/setupDatabases.ts
@@ -3,6 +3,7 @@ export default () => {
     const connect = async () => {
       try {
         await mongoose.connect('mongodb://localhost:27017/legbook-backend');
+        console.log('Connected to database successfully');
       } catch (error) {
         console.error('Error connecting to database', error);
         process.exit(1);
