e1afbf2a3718b80b526076562a1bb0381335d913
Hungle2911 <122929293+Hungle2911@users.noreply.github.com>
2024-11-04 15:34:37 -0500

    feat: implement global error handler

--- src/setupServer.ts
+++ src/setupServer.ts
@@ -13,6 +13,7 @@ import { createAdapter } from "@socket.io/redis-adapter";
 import "express-async-errors"
 import { config } from "./config";
 import applicationRouter from "./routes";
+import { CustomError, IErrorResponse } from "./shared/globals/helpers/error-handler";
 
 const SERVER_PORT = 8000;
 export class LegBookServer {
@@ -59,7 +60,19 @@ export class LegBookServer {
     applicationRouter(app);
   }
 
-  private globalErrorHandler(app: Application): void {}
+  private globalErrorHandler(app: Application): void {
+    app.all('*', (req: Request, res: Response) => {
+      res.status(HTTP_STATUS.NOT_FOUND).json({ message: `${req.originalUrl} not found` });
+    });
+
+    app.use((error: IErrorResponse, _req: Request, res: Response, next: NextFunction) : any => {
+      console.error(error);
+      if (error instanceof CustomError) {
+        return res.status(error.statusCode).json(error.serializeErrors());
+      }
+      next();
+    });
+  }
 
   private async startServer(app: Application): Promise<void> {
     try {
